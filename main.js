9fc91281de3d85234a1f96078255f3133b8e7946
Kim Schlesinger <schlesinger.kim@gmail.com>
2016-03-16 16:35:33 -0600

    added code to change background color of specified elements

--- main.js
+++ main.js
@@ -1,42 +1,59 @@
-// var firstParagraph = document.getElementById('main');
-// console.log(firstParagraph);
-//
-//
-// var getAllElements = document.getElementsByTagName('ul');
-// console.log(getAllElements);
-//
-// var getSingleElement = document.getElementsByTagName('ul')[0];
-// console.log(getSingleElement);
-//
-// var getListItems = document.getElementsByTagName('li');
-// console.log(getListItems);
-//
-// for (var i = 0; i <getListItems.length; i++) {
-//   console.log(getListItems[i]);
-// }
-//
-// var evenElements = document.getElementsByClassName('even');
-// console.log(evenElements);
-//
-// var firstEvenElement = document.getElementsByClassName('even')[0];
-// console.log(firstEvenElement);
-//
-// var oddElements = document.getElementsByClassName('odd');
-// console.log(oddElements);
-//
-// var firstOddElement = document.getElementsByClassName('odd')[1];
-// console.log(firstOddElement);
-//
-//
-// for (var i = 0 ; i < oddElements.length; i++) {
-//   console.log(oddElements[i]);
-// }
-//
-// var main = document.querySelector('#main')
-// console.log(main);
-//
-// var firstOddElement = document.querySelector('.odd');
-// console.log(firstOddElement);
+console.log(firstParagraph);
+var firstParagraph = document.getElementById('main');
 
-// var oddAndEvenElements= document.querySelectorAll('.odd, .even');
-// console.log(oddAndEvenElements);
+
+var getAllElements = document.getElementsByTagName('ul');
+console.log(getAllElements);
+
+var getSingleElement = document.getElementsByTagName('ul')[0];
+console.log(getSingleElement);
+
+var getListItems = document.getElementsByTagName('li');
+console.log(getListItems);
+
+for (var i = 0; i <getListItems.length; i++) {
+  console.log(getListItems[i]);
+}
+
+for (var i = 0; i < getListItems.length; i++) {
+  console.log('LOOK AT THIS! ' + getListItems[i].innerHTML);
+}
+
+
+var evenElements = document.getElementsByClassName('even');
+console.log(evenElements);
+
+var firstEvenElement = document.getElementsByClassName('even')[0];
+console.log(firstEvenElement);
+
+var oddElements = document.getElementsByClassName('odd');
+console.log(oddElements);
+
+var firstOddElement = document.getElementsByClassName('odd')[1];
+console.log(firstOddElement);
+
+firstOddElement.style.fontSize = "100px";
+
+
+for (var i = 0 ; i < oddElements.length; i++) {
+  console.log(oddElements[i]);
+}
+
+var main = document.querySelector('#main')
+console.log(main);
+
+//change the background color of the #main
+document.getElementById('main').style.backgroundColor = "peachpuff";
+
+var firstOddElement = document.querySelector('.odd');
+console.log(firstOddElement);
+
+firstOddElement.style.backgroundColor = "purple";
+firstOddElement.style.color = "#fff";
+
+
+var oddAndEvenElements= document.querySelectorAll('.odd, .even');
+console.log(oddAndEvenElements);
+
+var listNode = document.getElementById('list')
+console.log(listNode);
